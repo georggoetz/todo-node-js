93654c555c778f78f46a04d95aa5d08a3c8d82db
Georg Götz <georg.goetz@haufe-lexware.com>
2019-10-18 21:51:57 +0200

    Renamed environment variables

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 var pgp = require('pg-promise')()
 
 var db = pgp({
-  host: process.env.TODO_HOST || 'localhost',
-  port: process.env.TODO_PORT || 5432,
+  host: process.env.TODO_DB_HOST || 'localhost',
+  port: process.env.TODO_DB_PORT || 5432,
   database: process.env.TODO_DB_NAME || 'todo-db',
   user: process.env.TODO_DB_USER || 'postgres',
   password: process.env.TODO_DB_PASS || 'Lexware12345'
